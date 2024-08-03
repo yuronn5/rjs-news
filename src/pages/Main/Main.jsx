13c7e53e304e5576d81153185f0ff59ed13a137a
yurii bilous <yurii.b@elogic.com.ua>
2024-08-03 20:21:27 +0200

    added useefect for rendering news on the page

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./styles.module.css";
 import NewsBanner from "../../components/NewsBanner/NewsBanner";
+import { getNews } from "../../api/apiNews";
+import { useState } from "react";
 
 const Main = () => {
+    const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await getNews();
+        setNews(response.news);
+        console.log(response.news);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchNews();
+  }, []);
   return (
     <main className={styles.main}>
-      <NewsBanner />
+      {news.length > 0 ? <NewsBanner item={news[8]} /> : null}
     </main>
   );
 };
