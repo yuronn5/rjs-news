f11d913572e1fbd29d2709ab61a5b2c59ae90247
yurii bilous <yurii.b@elogic.com.ua>
2024-08-07 17:53:31 +0200

    added pagination for our project

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const Pagination = ({ totalPages, handlePageClick, handlePrevPage, handleNextPage, currentPage  }) => {
+  return (
+    <div className={styles.pagination}>
+      <button disabled={currentPage <= 1} onClick={handlePrevPage} className={styles.arrow}>{"<"}</button>
+      <div className={styles.list}>
+        {[...Array(totalPages)].map((_, index) => {
+          return (
+            <button disabled={currentPage === index + 1} onClick={() => handlePageClick(index + 1)} className={styles.pageNumber} key={index}>
+              {index + 1}
+            </button>
+          );
+        })}
+      </div>
+      <button disabled={currentPage >= totalPages} onClick={handleNextPage} className={styles.arrow}>{">"}</button>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,30 +5,77 @@ import { getNews } from "../../api/apiNews";
 import { useState } from "react";
 import NewsList from "../../components/NewsList/NewsList";
 import Skeleton from "../../components/Skeleton/Skeleton";
+import Pagination from "../../components/Pagination/Pagination";
 
 const Main = () => {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 10;
+  const pageSize = 10;
+
+  const fetchNews = async (currentPage) => {
+    try {
+      setIsLoading(true);
+      const response = await getNews(currentPage, pageSize);
+      setNews(response.news);
+      console.log(response.news);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        setIsLoading(true);
-        const response = await getNews();
-        setNews(response.news);
-        console.log(response.news);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+    fetchNews(currentPage);
+  }, [currentPage]);
 
-    fetchNews();
-  }, []);
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handlePageClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <main className={styles.main}>
-      {news.length > 0 && !isLoading ? <NewsBanner item={news[0]} /> : <Skeleton count={1} type="banner" />}
-      {!isLoading ? (<NewsList news={news} />) : (<Skeleton count={10} type="item" />)}
+      {news.length > 0 && !isLoading ? (
+        <NewsBanner item={news[0]} />
+      ) : (
+        <Skeleton count={1} type="banner" />
+      )}
+
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        totalPages={totalPages}
+        handleNextPage={handleNextPage}
+        handlePrevPage={handlePrevPage}
+        handlePageClick={handlePageClick}
+      />
+      {!isLoading ? (
+        <NewsList news={news} />
+      ) : (
+        <Skeleton count={10} type="item" />
+      )}
+
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        totalPages={totalPages}
+        handleNextPage={handleNextPage}
+        handlePrevPage={handlePrevPage}
+        handlePageClick={handlePageClick}
+      />
     </main>
   );
 };
