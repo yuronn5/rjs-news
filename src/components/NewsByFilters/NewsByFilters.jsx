52cade2b33dc3f89308c98d34bcb7cc61f94e6fe
yurii bilous <yurii.b@elogic.com.ua>
2024-08-13 11:21:24 +0200

    wrapper for pagination

--- src/components/NewsByFilters/NewsByFilters.jsx
+++ src/components/NewsByFilters/NewsByFilters.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./styles.module.css";
-import Pagination from "../Pagination/Pagination";
 import NewsList from "../NewsList/NewsList";
 import { TOTAL_PAGES } from "../../constants/constants";
 import NewsFilters from "../NewsFilters/NewsFilters";
@@ -9,6 +8,7 @@ import { getNews } from "../../api/apiNews";
 import { useDebounce } from "../../helpers/hooks/useDebounce";
 import { useFilters } from "../../helpers/hooks/useFilters";
 import { PAGE_SIZE } from "../../constants/constants";
+import PaginationWrapper from "../PaginationWrapper/PaginationWrapper";
 
 const NewsByFilters = () => {
   const { filters, changeFilter } = useFilters({
@@ -44,24 +44,18 @@ const NewsByFilters = () => {
   return (
     <section className={styles.section}>
       <NewsFilters changeFilter={changeFilter} filters={filters} />
-      
-      <Pagination
+
+      <PaginationWrapper
+        top={true}
+        bottom={true}
         currentPage={filters.page_number}
         totalPages={TOTAL_PAGES}
         handleNextPage={handleNextPage}
         handlePrevPage={handlePrevPage}
         handlePageClick={handlePageClick}
-      />
-
-      <NewsList isLoading={isLoading} news={data?.news} />
-
-      <Pagination
-        currentPage={filters.page_number}
-        totalPages={TOTAL_PAGES}
-        handleNextPage={handleNextPage}
-        handlePrevPage={handlePrevPage}
-        handlePageClick={handlePageClick}
-      />
+      >
+        <NewsList isLoading={isLoading} news={data?.news} />
+      </PaginationWrapper>
     </section>
   );
 };
