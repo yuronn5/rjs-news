934344f915138be6f28e9d388f0757462f217f11
yurii bilous <yurii.b@elogic.com.ua>
2024-08-13 12:02:12 +0200

    fix slider and add arrows

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import styles from "./styles.module.css";
+import { forwardRef } from "react";
 
-const Categories = ({ categories, setSelectedCategory, selectedCategory }) => {
-  return (
-    <div className={styles.categories}>
-      <button
-        onClick={() => setSelectedCategory(null)}
-        className={!selectedCategory ? styles.active : styles.item}
-      >
-        All
-      </button>
-      {categories.map((category) => {
-        return (
-          <button
-            className={
-              selectedCategory === category ? styles.active : styles.item
-            }
-            onClick={() => setSelectedCategory(category)}
-            key={category}
-          >
-            {category}
-          </button>
-        );
-      })}
-      ;
-    </div>
-  );
-};
+const Categories = forwardRef(
+  ({ categories, setSelectedCategory, selectedCategory }, ref) => {
+    return (
+      <div ref={ref} className={styles.categories}>
+        <button
+          onClick={() => setSelectedCategory(null)}
+          className={!selectedCategory ? styles.active : styles.item}
+        >
+          All
+        </button>
+        {categories.map((category) => {
+          return (
+            <button
+              onClick={() => setSelectedCategory(category)}
+              className={
+                selectedCategory === category ? styles.active : styles.item
+              }
+              key={category}
+            >
+              {category}
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
+);
+
+Categories.displayName = "Categories";
 
 export default Categories;
