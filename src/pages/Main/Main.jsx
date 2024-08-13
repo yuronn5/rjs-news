cef43b4301c19776da9a65b068bb390deb4ccb81
yurii bilous <yurii.b@elogic.com.ua>
2024-08-13 10:31:09 +0200

    removed commented code from main component

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import styles from "./styles.module.css";
-import NewsBanner from "../../components/NewsBanner/NewsBanner";
-import { getCategories, getNews } from "../../api/apiNews";
-import NewsList from "../../components/NewsList/NewsList";
-import Pagination from "../../components/Pagination/Pagination";
-import Categories from "../../components/Categories/Categories";
-import Search from "../../components/Search/Search";
+import { getNews } from "../../api/apiNews";
 import { useDebounce } from "../../helpers/hooks/useDebounce";
-import { PAGE_SIZE, TOTAL_PAGES } from "../../constants/constants";
+import { PAGE_SIZE } from "../../constants/constants";
 import { useFetch } from "../../helpers/hooks/useFetch";
 import { useFilters } from "../../helpers/hooks/useFilters";
 import LatestNews from "../../components/LatestNews/LatestNews";
@@ -28,60 +23,16 @@ const Main = () => {
     keywords: debouncedKeywords,
   });
 
-  // const { data: dataCategories } = useFetch(getCategories);
-
-  // const handleNextPage = () => {
-  //   if (filters.page_number < TOTAL_PAGES) {
-  //     changeFilter("page_number", filters.page_number + 1);
-  //   }
-  // };
-
-  // const handlePrevPage = () => {
-  //   if (filters.page_number > 1) {
-  //     changeFilter("page_number", filters.page_number - 1);
-  //   }
-  // };
-
-  // const handlePageClick = (pageNumber) => {
-  //   changeFilter("page_number", pageNumber);
-  // };
-
   return (
     <main className={styles.main}>
-      <LatestNews banners={data && data.news } isLoading={isLoading} />
+      <LatestNews banners={data && data.news} isLoading={isLoading} />
 
-      <NewsByFilters filters={filters} changeFilter={changeFilter} isLoading={isLoading} news={data?.news} />
-
-      {/* {dataCategories ? (
-        <Categories
-          categories={dataCategories.categories}
-          setSelectedCategory={(category) => changeFilter("category", category)}
-          selectedCategory={filters.category}
-        />
-      ) : null}
-
-      <Search
-        keywords={filters.keywords}
-        setKeywords={(keywords) => changeFilter("keywords", keywords)}
+      <NewsByFilters
+        filters={filters}
+        changeFilter={changeFilter}
+        isLoading={isLoading}
+        news={data?.news}
       />
-
-      <Pagination
-        currentPage={filters.page_number}
-        totalPages={TOTAL_PAGES}
-        handleNextPage={handleNextPage}
-        handlePrevPage={handlePrevPage}
-        handlePageClick={handlePageClick}
-      />
-
-      <NewsList isLoading={isLoading} news={data?.news} />
-
-      <Pagination
-        currentPage={filters.page_number}
-        totalPages={TOTAL_PAGES}
-        handleNextPage={handleNextPage}
-        handlePrevPage={handlePrevPage}
-        handlePageClick={handlePageClick}
-      /> */}
     </main>
   );
 };
