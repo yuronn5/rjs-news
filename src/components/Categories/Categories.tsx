03ce2e58ed0249b3337c478901e912c9d7029f1f
yurii bilous <yurii.b@elogic.com.ua>
2024-08-17 12:45:16 +0200

    categories ts

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import styles from "./styles.module.css";
 import { forwardRef } from "react";
+import { ForwardedRef } from "react";
+import { CategoriesType } from "../../interfaces";
+
+interface Props {
+  categories: CategoriesType[];
+  selectedCategory: CategoriesType | null;
+  setSelectedCategory: (category: CategoriesType | null) => void;
+}
 
 const Categories = forwardRef(
-  ({ categories, setSelectedCategory, selectedCategory }, ref) => {
+  (
+    { categories, setSelectedCategory, selectedCategory }: Props,
+    ref: ForwardedRef<HTMLDivElement>
+  ) => {
     return (
       <div ref={ref} className={styles.categories}>
         <button
