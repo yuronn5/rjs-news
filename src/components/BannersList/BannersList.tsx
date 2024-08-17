4d11f9c70864683ecb4c355277b6501a07258198
yurii bilous <yurii.b@elogic.com.ua>
2024-08-17 12:17:46 +0200

    banners list tsx

--- src/components/BannersList/BannersList.tsx
+++ src/components/BannersList/BannersList.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import styles from "./styles.module.css";
 import withSkeleton from "../../helpers/hocs/withSkeleton";
 import NewsBanner from "../NewsBanner/NewsBanner";
+import { INews } from "../../interfaces";
 
-const BannersList = ({ banners }) => {
+interface Props {
+  banners?: INews[];
+}
+
+const BannersList = ({ banners }: Props) => {
   return (
     <ul className={styles.banners}>
       {banners?.map((banner) => {
