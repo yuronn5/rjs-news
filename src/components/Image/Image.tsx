012042a2f6472aa432d645a4753f8a7dc6eae9ef
yurii bilous <yurii.b@elogic.com.ua>
2024-08-17 12:21:11 +0200

    image ts

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,13 +1,15 @@
-import React from "react";
 import styles from "./styles.module.css";
 
+interface Props {
+  image: string;
+}
 
-const Image = ({ image }) => {
+const Image = ({ image }: Props) => {
   return (
     <div className={styles.wrapper}>
-     {image ? <img className={styles.image} src={image} alt="news" /> : null}
+      {image ? <img className={styles.image} src={image} alt="news" /> : null}
     </div>
   );
 };
- 
+
 export default Image;
