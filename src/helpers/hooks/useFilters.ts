c1b2b7780501e442c04a4409669c0b68fc7a1d2c
yurii bilous <yurii.b@elogic.com.ua>
2024-08-17 12:37:41 +0200

    usefilters ts

--- src/helpers/hooks/useFilters.ts
+++ src/helpers/hooks/useFilters.ts
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import { IFilters } from "../../interfaces";
 
-export const useFilters = (initialFilters) => {
-  const [filters, setFilters] = useState(initialFilters);
+export const useFilters = (initialFilters: IFilters) => {
+  const [filters, setFilters] = useState<IFilters>(initialFilters);
 
-  const changeFilter = (key, value) => {
+  const changeFilter = (key: string, value: string | number | null) => {
     setFilters((prev) => {
       return { ...prev, [key]: value };
     });
