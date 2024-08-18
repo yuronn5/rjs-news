f289bc64b77e1f565d58d6a5927f68ad52d90e1c
yurii bilous <yurii.b@elogic.com.ua>
2024-08-18 13:11:33 +0200

    added selector to check if everything write

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Header from "./components/Header/Header";
 import { useTheme } from "./context/ThemeContext";
 import Main from "./pages/Main/Main";
+import { useAppSelector } from "./store";
 
 function App() {
   const { isDark } = useTheme();
+  const news = useAppSelector((state) => state.news.news);
+  console.log(news);
 
   return (
     <div className={`app ${isDark ? "dark" : "light"}`}>
